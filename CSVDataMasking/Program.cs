d6fec1c60c4fcf6d5d73439ea8fe3180c44bcd24
André Jager <andre.jager@orion6.nl>
2023-05-04 11:37:17 +0200

    Remove dependency on Newtonsoft.Json

--- CSVDataMasking/Program.cs
+++ CSVDataMasking/Program.cs
@@ -4,50 +4,41 @@
 //
 //
 
-#region
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
+using System.Text.Json;
 
-#endregion
+const char separator = ';';
 
-var separator = ',';
+var historyFileName = @"c:\temp\history.txt";
+if (!File.Exists(args[0])) throw new Exception("Input file not found");
 
-var historyFileName = "history.txt";
-if (!File.Exists(args[0]))
-{
-    throw new Exception("Input file not found");
-}
-
-if (File.Exists(args[1]))
-{
-    throw new Exception("Output file exits");
-}
+if (File.Exists(args[1])) throw new Exception("Output file exits");
 
 // Get an enumeration of all strings in the file.
-var payload = File.ReadLines(
-    args[0]
-  , Encoding.Latin1);
+var payload = File.ReadLines(args[0], Encoding.Latin1);
+
+if (payload == null)
+    throw new Exception("Input file is empty");
 
 // Determine the headers.
-var Headers =
+var headers =
     payload
         .First()
         .Split(separator);
 
 // Read the headers of the columns to be masked.
-var MaskHeaders =
+var maskHeaders =
     File
-        .ReadLines("mask-header.txt");
+        .ReadLines(@"c:\temp\mask-header.txt");
 
 // Determine the column number of the headers
-var columnNumbers = Headers
+var columnNumbers = headers
     .Select((_, index) => index)
-    .Where(index => MaskHeaders.Contains(Headers[index]));
+    .Where(index => maskHeaders.Contains(headers[index]));
 
 // Create a dictionary where we store the original value and the masked value
 // combination.
@@ -57,16 +48,14 @@ var maskedValues = new Dictionary<string, string>();
 // test data.
 
 if (File.Exists(historyFileName))
-{
-    maskedValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(
+    maskedValues = JsonSerializer.Deserialize<Dictionary<string, string>>(
         File.ReadAllText(historyFileName));
-}
 
 foreach (var columnNumber in columnNumbers)
 {
     // Determine all distinct values in the column
 
-    var ColumnValues =
+    var columnValues =
         payload
             .Skip(1) // skip header record
             .Select(item => item.Split(separator)) // split record in columns
@@ -76,13 +65,11 @@ foreach (var columnNumber in columnNumbers)
     var counter = maskedValues.Count;
 
     // For all found distinct column values, we generate a masked value
-    foreach (var columnValue in ColumnValues)
+    foreach (var columnValue in columnValues)
         if (!maskedValues.ContainsKey(columnValue))
-        {
             maskedValues.Add(
                 columnValue // The original column value
-              , $"{Headers[columnNumber]} {counter++:0000}"); // the masked value
-        }
+                , $"{headers[columnNumber]} {counter++:0000}"); // the masked value
 }
 
 // As of here, the maskedValues dictionary contains the original column content (key)
@@ -91,18 +78,16 @@ foreach (var columnNumber in columnNumbers)
 // Now replace all original content with the masked value and write out to file.
 File.WriteAllText(
     args[1]
-  , maskedValues.Aggregate(
+    , maskedValues.Aggregate(
         File.ReadAllText(
             args[0]
-          , Encoding.Latin1)
-      , (current, kv) => current.Replace(
+            , Encoding.Latin1)
+        , (current, kv) => current.Replace(
             kv.Key
-          , kv.Value)));
+            , kv.Value)));
 
 // Write out the obfuscated values for future reference.
 
 File.WriteAllText(
     historyFileName
-  , JsonConvert.SerializeObject(
-        maskedValues
-      , (Formatting) System.Xml.Formatting.Indented));
+    , JsonSerializer.Serialize(maskedValues));
